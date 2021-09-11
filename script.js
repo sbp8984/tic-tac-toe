708606b27103d92c7096e42f80a592ac1910212d
sbp8984 <87753278+sbp8984@users.noreply.github.com>
2021-09-11 13:15:57 +0100

    removed example x and circle in index

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+let circleTurn
+
 const cellElements = document.querySelectorAll('[data-cell]')
 
 cellElements.forEach(cell => {
@@ -5,5 +9,13 @@ cellElements.forEach(cell => {
 })
 
 function handleClick(e) {
-    console.log('clicked')
+    const cell = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+    //place Mark
+    // Check for Win
+    // Check for Draw
+    // Switch Turns
 }
+
+function placeMark (cell, c)
